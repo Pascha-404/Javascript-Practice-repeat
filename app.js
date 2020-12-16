e64d7f9b59dee72ec81ade012e68d9c884b9b825
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-17 00:08:21 +0100

    added sumArray

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ function capitalize(input){
 let word = input;
 return word[0].toUpperCase() + word.slice(1);
 }
+
+function sumArray(numb){
+    let total = 0;
+    for (let i = 0; i < numb.length; i++){
+        total += numb[i];
+    }
+    return total;
+}
