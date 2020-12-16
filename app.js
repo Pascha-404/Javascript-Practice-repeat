2768ea423b6f82e0d4152aeda635297fbd0afe63
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-16 23:35:28 +0100

    First repeat

---
+++ app.js
@@ -0,0 +1,13 @@
+function lastElement (argu){
+    if (argu.length === 0) {
+        return null;
+    } else {
+        for (let i = 0; i < argu.length; i++){
+            return argu[argu.length - 1]
+        }
+    }
+}
+
+function capitalize(word){
+    
+}
