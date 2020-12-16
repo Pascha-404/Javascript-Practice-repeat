d482470cb2d20855efb36d423b6669f48713c97a
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-17 00:43:25 +0100

    added returnDay

--- app.js
+++ app.js
@@ -20,3 +20,12 @@ function sumArray(numb){
     }
     return total;
 }
+
+function returnDay(day){
+    if (day < 1 | day > 7){
+        return null;
+    } else {
+        const weekDays = ['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+            return weekDays[day-1];
+    }
+}
