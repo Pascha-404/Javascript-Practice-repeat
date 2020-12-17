10574fbcc1cb54b5df1b56af1c26a544bc8a8f44
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-17 21:26:28 +0100

    worked with 'this'

--- methods.js
+++ methods.js
@@ -1,7 +1,18 @@
 const square = {
     area(side) {
         return side * side;
-    }, perimeter(side){
+    },
+    perimeter(side) {
         return side * 4;
     }
+}
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount++;
+        return 'EGG'
+    }
+
 }
