1255c0a9c606aeb8fef20971e11a784c9d3cad76
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-18 16:53:31 +0100

    repeated exercises

---
+++ appRepeat2.js
@@ -0,0 +1,30 @@
+function capitalize(input) {
+    let word = input;
+    return word[0].toUpperCase() + word.slice(1);
+}
+
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    }
+    return false;
+}
+
+function returnDay(day) {
+    if (day >= 1 && day <= 7) {
+        const week = ['Mond', 'Tues', 'Wedn', 'Thur', 'Frid', 'Sat', 'Sund'];
+        return week[day - 1]
+    } else {
+        if (day === 0) {
+            return null
+        }
+
+    }
+}
+
+function sumArray(number){
+    let total = 0;
+    for (let i = 0; i < number.length; i++){
+        total += number[i];
+    } return total
+}
