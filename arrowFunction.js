3876a86c9843336a8d166d9438fb123f30049df9
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-18 18:58:14 +0100

    tested reduce

--- arrowFunction.js
+++ arrowFunction.js
@@ -2,6 +2,16 @@ const greet = person => {
     return `Hey ${person}`;
 }
 
-const validUserNames = name => name.filter(user => user.length<10);
+const validUserNames = name => name.filter(user => user.length < 10);
 
-const allEvens = even => even.every(numb => numb %2 === 0)
+const allEvens = even => even.every(numb => numb % 2 === 0)
+
+const prices = [2, 3.77, 23.40, 9.50, 40.30, 8, 14.66];
+
+const totalPrice = prices.reduce((total, price) => total + price, 7);
+
+const mostExp = prices.reduce((mostExpensive, nextItem) => {
+if (mostExpensive > nextItem){
+    return mostExpensive;
+} return nextItem
+})
