3c6e4f0e6ab1b91d1ac63584c5baccda2d7c188d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-20 00:28:25 +0100

    added layAnEgg

--- repeat.js
+++ repeat.js
@@ -2,4 +2,19 @@ const names = ['jason   ', 'sisiphos   ', 'join eltoon   ', '    capital bla']
 
 const cleanNames = names => names.map(namesedit => namesedit.trim())
 
-const greet = person => {return `Hey ${person}!`}
+const greet = person => {
+    return `Hey ${person}!`
+}
+
+const validUserNames = user => user.filter(u => u.length < 10)
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+layAnEgg() {
+    this.eggCount++;
+    return 'EGG'
+}
+}
+
+// const layAnEgg = () => (hen.eggCount++, 'EGG')
